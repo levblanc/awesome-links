80d004f330d91f45878e103fd2492403c1c335d5
levblanc <levblanc@gmail.com>
2022-12-22 21:43:29 +0800

    Add ApolloClient to the front end

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import Head from 'next/head';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '../lib/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,9 +15,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <Component {...pageProps} />
-      </MantineProvider>
+      <ApolloProvider client={apolloClient}>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ApolloProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,84 @@
-import { Button } from '@mantine/core';
+import {
+  Button,
+  LoadingOverlay,
+  Alert,
+  Card,
+  Image,
+  Text,
+  Group,
+  Badge,
+} from '@mantine/core';
+import { gql, useQuery } from '@apollo/client';
+import { IconAlertCircle } from '@tabler/icons';
+
+const AllLinksQuery = gql`
+  query {
+    links {
+      id
+      title
+      url
+      description
+      imageUrl
+      category
+    }
+  }
+`;
 
 function Home() {
+  const { data, error, loading } = useQuery(AllLinksQuery);
+
+  console.log('data', data);
   return (
     <>
-      <Button>Test Button</Button>
+      {error ? (
+        <Alert
+          icon={<IconAlertCircle size={16} />}
+          title='Error Occured!'
+          color='red'
+        >
+          {error.message}
+        </Alert>
+      ) : (
+        <>
+          <LoadingOverlay visible={loading} overlayBlur={2} />
+          {data &&
+            data.links.length &&
+            data.links.map((item, index: number) => {
+              return (
+                <Card
+                  sx={{ width: 320, margin: 10 }}
+                  shadow='sm'
+                  p='lg'
+                  radius='md'
+                  withBorder
+                  key={index}
+                >
+                  <Card.Section>
+                    <Image src={item.imageUrl} height={240} alt={item.title} />
+                  </Card.Section>
+                  <Group position='apart' mt='md' mb='xs'>
+                    <Text weight={500}>{item.title}</Text>
+                    <Badge color='pink' variant='light'>
+                      {item.category}
+                    </Badge>
+                  </Group>
+                  <Text size='sm' color='dimmed'>
+                    {item.description}
+                  </Text>
+                  <Button
+                    variant='light'
+                    color='blue'
+                    fullWidth
+                    mt='md'
+                    radius='md'
+                  >
+                    Bookmark
+                  </Button>
+                </Card>
+              );
+            })}
+        </>
+      )}
     </>
   );
 }
